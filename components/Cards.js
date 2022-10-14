78472ed1593c24d400e50d64a8bd51fb39304e49
Santiago Calligari <89709491+SantiagoCalligari@users.noreply.github.com>
2022-10-13 22:54:46 -0300

    Las cartas se adaptan a la pantalla

--- components/Cards.js
+++ components/Cards.js
@@ -3,15 +3,18 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
-
+import styles from '../styles/Home.module.css';
 export default function Cards({ titulo, cuerpo, imagen }) {
     const ruta = imagen.replace(/\.[^/.]+$/, "");
     const nombreImagen=ruta;
+
+
+
+    //TODO no deberia ser una ruta. Usar Componente MODAL cuando toca la carta.
     return (
-    <Card sx={{ width: 250, height: 'auto' }}>
+    <Card className={styles.cartita} >
         <CardActionArea href={ruta}>
             <CardMedia
                 component="img"
--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
   {
     async function tomarBares()
     {
-        const apiUrlGetData = 'http://localhost:3000/api/getSqlData'; //TODO esto, deberia tener el link de la pagina de schujamn, o la mia.
+        const apiUrlGetData = '../api/getSqlData';
         const response = await fetch(apiUrlGetData);
         const res = await response.json();
         setdataResponse(res.bares);
@@ -25,7 +25,7 @@ export default function Home() {
   <main className={styles.main}>
     <Cabecera titulo="La Merendola!" />
     <div className={styles.cardContainer}>
-      {dataResponse.map((bar) => <Cards key={bar.Titulo} titulo={bar.Titulo} cuerpo={bar.Cuerpo} imagen={bar.Imagen}/>)}
+      {dataResponse.map((bar) => <Cards key={bar.Titulo} titulo={bar.Titulo} cuerpo={bar.Cuerpo} imagen={bar.Imagen}/>)} 
       {dataResponse.map((bar) => <Cards key={bar.Titulo} titulo={bar.Titulo} cuerpo={bar.Cuerpo} imagen={bar.Imagen}/>)}
       {dataResponse.map((bar) => <Cards key={bar.Titulo} titulo={bar.Titulo} cuerpo={bar.Cuerpo} imagen={bar.Imagen}/>)}
     </div>
